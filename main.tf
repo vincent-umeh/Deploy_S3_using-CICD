3096092cd3122e991859f627f0a8b70f282460c4
vincent-umeh <vincent.umehj@gmail.com>
2025-04-28 03:35:49 +0100

    updated terrofrm enabling public access

--- main.tf
+++ main.tf
@@ -29,10 +29,10 @@ resource "aws_s3_bucket_versioning" "versioning" {
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # Enable server-side encryption
